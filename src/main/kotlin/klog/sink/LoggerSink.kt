b3d6bf64b020af5e61ddc7ca6951dde4ef6ec934
Shemnei <Shemnei@users.noreply.github.com>
2018-06-20 19:56:01 +0200

    changed some names and made FormattedLogSink an abc

--- src/main/kotlin/klog/sink/LoggerSink.kt
+++ src/main/kotlin/klog/sink/LoggerSink.kt
@@ -5,12 +5,13 @@ import klog.Logger
 import klog.filter.LogFilter
 
 // TODO: 12.06.2018 maybe find a better way to relay loggers
+// // FIXME: 20.06.2018 args wont get relayed
 class LoggerSink(
         val logger: Logger
 ) : LogSink {
     override val filter: MutableSet<LogFilter> = mutableSetOf()
 
-    override fun processLog(logRecord: LogRecord) {
-        logger.log(logRecord.level, logRecord.message!!, logRecord.throwable, logRecord.marker)
+    override fun processLog(log: LogRecord) {
+        logger.log(log.level, log.message!!, log.throwable, log.marker)
     }
 }
